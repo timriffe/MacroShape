7e8026d258b3f215fc4e0be4030039ce9ccaeb15
Tim Riffe <tim.riffe@gmail.com>
2018-05-24 10:46:21 +0200

    testing at higher grid resolution

--- MacroShape/DR/Manuscript/R/Application.R
+++ MacroShape/DR/Manuscript/R/Application.R
@@ -170,3 +170,51 @@ for (i in 1:nrow(Fem)){
 dev.off()
 
 
+# experiment to see how fine the grid can be
+Dat          <- data.table(Dat)
+Dat$SD       <- sqrt(Dat$Var)
+DatMod       <- Dat[,get_brr_dt(.SD,xvar="ex",yvar="SD"),by=list(Sex, Year, Age)]
+DatMod       <- data.frame(DatMod)
+Fem          <- DatMod[DatMod$Sex == "f", ]
+
+par(mai=c(.3,.3,.1,.1))
+plot(NULL, type = "n", xlim = c(1950,2015), ylim = c(0,105), axes = FALSE, xlab = "", ylab = "", asp = 1, 
+		panel.first = list(
+				rect(1950,0,2015,105,border = NA,col = gray(.92)),
+				segments(seq(1950,2015,by=5),0,seq(1950,2015,by=5),105,col="white"),
+				segments(1950,seq(0,105,by=5),2015,seq(0,105,by=5),col="white"),
+				text(seq(1950,2010,by=10),0,seq(1950,2010,by=10),pos=1,xpd=TRUE,cex=1.2),
+				text(1950,seq(0,100,by=10),seq(0,100,by=10),pos=2,xpd=TRUE,cex=1.2)))
+# optional background- more crowded out here
+#image(1950.5:2012.5, .5:104.5,
+#		t(CV),
+#		breaks = breaks,
+#		col = ramp(length(breaks)-1),
+#		xlim=c(1950,2013),
+#		ylim=c(0,105),
+#		asp=1,
+#		add=TRUE)
+#contour(1950.5:2012.5, .5:104.5,
+#		t(CV),
+#		breaks = pretty(CV,10),
+#		xlim=c(1950,2013),
+#		ylim=c(0,105),
+#		asp=1,
+#		add=TRUE)
+Int <- 3 # so that segment midpoint crosses cell centroid, even though the cell size for rendering
+# has been expanded.
+for (i in 1:nrow(Fem)){
+	x <- Fem[i,]
+	draw_field_element(
+			age = x$Age - (Int-1)/2,     # lower bound of implied cell age
+			year = x$Year - (Int-1)/2,   # lower bound of implied cell year
+			interval = Int,  # cell dimension
+			slope = x$b,  # slope of regression or whatever
+			length = x$iqr/3, # default meaning = interval - (2*pad)
+			pad = 0,   # edge pad if length = 1 and slope = 0 or Inf
+			lambda =  2,
+			col = gray(grayrange[2]-x$rsq*diff(grayrange)+grayrange[1]),
+			lwd = .5 + x$rsq,
+			xpd=TRUE)
+}
+
